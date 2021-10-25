ec3565ac7d6ca90ec3aa549dc1f84e96c7ebcf01
Einra11 <34428690+einra11@users.noreply.github.com>
2021-10-25 17:49:49 +0800

    eeee

--- Contents/js/memberspage.js
+++
@@ -1,7 +0,0 @@
-$(function() {
-    if (localStorage.getItem('GKKIS_Member_Details') != null) {
-        $('#mainFrame').attr('src', "Pages/membershome.html");
-    } else {
-        $('#mainFrame').attr('src', "Pages/memberslogin.html");
-    }
-})
--- Contents/js/welcomeadmin.js
+++
@@ -1,5 +0,0 @@
-$(function() {
-    if (localStorage.getItem('GKKIS_Admin_Details') != null) {
-        $('#welcome-admin .form-container.im-hidden').removeClass('im-hidden');
-    }
-})
